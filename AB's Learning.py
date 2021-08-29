acf81276c4aa547b7f0f3dd4a31c978dd67a0135
Abhay <87917570+Vnsabhay@users.noreply.github.com>
2021-08-29 16:06:42 -0700

    Initial commit

---
+++ AB's Learning.py
@@ -0,0 +1,6 @@
+Name = str(input("What's Your name? "))
+age = input("What's your age ? ")
+Newname = Name.split()
+Text = Name.capitalize() + " IS " + age + " Years old"
+print(Text.capitalize())
+print(Newname)
